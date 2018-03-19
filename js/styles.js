09ccf7186735120166c8d75fc6d6cfa564a129d9
Dawn Mott, kelsey guerra and Nick Kardish <dawn@>
2018-03-19 13:47:17 -0700

    created clickable site for Mustard and added questions

---
+++ js/styles.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+jQuery("h1").click(function() {
+  alert("That's a good boy!");
+});
+
+jQuery("p").click(function() {
+  alert("Mustard, SIT!");
+});
+
+jQuery("img").click(function() {
+  prompt ("Do you think Mustard is cute?")
+});
+});
