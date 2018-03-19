f23030413458ae98a3555c0134f9984eb0239b6e
Dawn Mott, kelsey guerra and Nick Kardish <nick@>
2018-03-19 14:18:32 -0700

    add clickable variables with sentences that return on the screen

--- js/styles.js
+++ js/styles.js
@@ -11,4 +11,16 @@ jQuery("p").click(function() {
 jQuery("img").click(function() {
   prompt ("Do you think Mustard is cute?")
 });
+jQuery("h2").click(function() {
+  var sentence = prompt("Enter a sentence about Mustard.")
+  var stringLength = sentence.length;
+  var firstLast = sentence.charAt(0) + sentence.charAt(stringLength - 1);
+  firstLast.toUpperCase();
+  alert(sentence);
+  var lastFirst = firstLast.charAt(0) + firstLast.charAt(1);
+  alert(lastFirst.toUpperCase());
+  var lastFirst = firstLast.charAt(1) + firstLast.charAt(0);
+  alert(lastFirst.toUpperCase());
+});
+
 });
